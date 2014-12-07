96514d0706a8d6f8027ca486d81762636f26411f
David Wen <dstwen@gmail.com>
2014-12-07 12:22:35 -0800

    add age and gender validations on patients

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,6 @@
 class Patient < ActiveRecord::Base
+  validates :age, :gender, presence: true
+  validates :gender, inclusion: %w(male female)
   validates :age, numericality: { greater_than_or_equal_to: 0 }
   validates :weight, numericality: { greater_than: 0 }
   validates :height, numericality: { greater_than: 0 }
