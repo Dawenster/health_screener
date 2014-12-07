6212e21f420dd9b9bec7785e80ae471331dcdb7d
David Wen <dstwen@gmail.com>
2014-12-07 14:11:50 -0800

    allow nil records for weight and height

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,6 +2,6 @@ class Patient < ActiveRecord::Base
   validates :age, :gender, presence: true
   validates :gender, inclusion: %w(male female)
   validates :age, numericality: { greater_than_or_equal_to: 0 }
-  validates :weight, numericality: { greater_than: 0 }
-  validates :height, numericality: { greater_than: 0 }
+  validates :weight, numericality: { greater_than: 0 }, :allow_nil => true
+  validates :height, numericality: { greater_than: 0 }, :allow_nil => true
 end
