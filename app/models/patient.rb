ad16ed3e2fa3b2a89fa7844cd6ab8f14c8a09ddc
David Wen <dstwen@gmail.com>
2014-12-07 12:07:04 -0800

    added patient db validations

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,5 @@
 class Patient < ActiveRecord::Base
+  validates :age, numericality: { greater_than_or_equal_to: 0 }
+  validates :weight, numericality: { greater_than: 0 }
+  validates :height, numericality: { greater_than: 0 }
 end
