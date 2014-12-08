39167b413c2a1df8e5747c941cbe7e245b55703f
linbilly <billy.lin@gmail.com>
2014-12-07 21:30:12 -0800

    fat-model screening_test#get_gender_string
    hell yes good programming practice

--- app/models/screening_test.rb
+++ app/models/screening_test.rb
@@ -5,4 +5,18 @@ class ScreeningTest < ActiveRecord::Base
     self.start_age ||= 0
     self.end_age ||= 200
   end
+
+
+  def get_gender_string
+    gender_string = "males or females"
+    case self.gender
+      when "male"
+        gender_string = "males"
+      when "female"
+        gender_string = "females"
+      else
+        gender_string = "males or females"   
+    end 
+  end
+
 end
