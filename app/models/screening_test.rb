add575cfdd173581294bc16ab62075e5ab464365
linbilly <billy.lin@gmail.com>
2014-12-07 21:39:51 -0800

    screening_test#get_age_range_string to show age ranges of test

--- app/models/screening_test.rb
+++ app/models/screening_test.rb
@@ -19,4 +19,25 @@ class ScreeningTest < ActiveRecord::Base
     end 
   end
 
+  def get_age_range_string
+    age_range_string = ""
+    case self.start_age
+    when 0
+      case self.end_age
+      when 200
+        age_range_string = "of all ages"
+      else
+        age_range_string = "below age "+self.end_age.to_s
+      end
+    else
+      case self.end_age
+      when 200
+        age_range_string = "above age "+self.start_age.to_s
+      else
+        age_range_string = "between ages of "+self.start_age.to_s+ " and "+self.end_age.to_s
+      end
+    end
+    return age_range_string
+  end
+
 end
